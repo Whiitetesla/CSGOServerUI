43a07ba6754edcdcb30d391bc5ffa7c3cd3fa442
Whiitetesla <whiite.h.l@gmail.com>
2017-09-19 11:14:59 +0200

    Added select cmd, select map (need to list all the maps and give it map codes), select number of players, update button and an update on program start

--- CSGO_UI/MainWindow.xaml.cs
+++ CSGO_UI/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -13,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace CSGO_UI
 {
@@ -21,33 +25,132 @@ namespace CSGO_UI
     /// </summary>
     public partial class MainWindow : Window
     {
-        public string SteamCDMPath { get; set; }
-        public string MapName { get; set; }
-        public int MaxPlayers { get; set; }
+        public string SteamCdmPath { get; set; }
+        public string MapCode { get; set; }
+        public int MaxPlayers { get; set; } = 10;
+        public DateTime LastUpdated { get; set; } = DateTime.Now;
+
+        public ObservableCollection<string> Maps { get; set; } = new ObservableCollection<string>();
         
 
         public MainWindow()
         {
             InitializeComponent();
-            Process myProcess = new Process();
+            UpdateClient();
+
+            setMaps();
+            MapCode = GetModes(Maps.First());
+        }
+
+        private void setMaps()
+        {
+            Maps.Add("Dust 2");
+            Maps.Add("Dust");
+            Maps.Add("Nuke");
+
         }
 
         //creates a string for the given gamemode
-        public string getModes(string mode)
+        public string GetModes(string mode)
         {
             string temp = "";
             switch (mode)
             {
-                case "": 
+                case "Dust 2":
+                    temp = "2Dust";
                     break;
-                case "1":
+                case "Dust":
+                    temp = "1Dust";
                     break;
-                case "2":
+                case "Nuke":
+                    temp = "deNuke";
                     break;
                 default:
+                    temp = "ckae";
                     break;
             }
             return temp;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            fileDialog.Filter = "Programer (.exe)|*.exe|Alle filer (*.*)|*.*";
+
+            fileDialog.FilterIndex = 1;
+
+            fileDialog.Multiselect = false;
+
+            bool? userClick = fileDialog.ShowDialog();
+
+            if (userClick == true)
+            {
+                string temp = System.IO.Path.GetFullPath(fileDialog.FileName);
+                int tempIndex = 0;
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    if (temp[i]== Char.Parse("\\"))
+                    {
+                        tempIndex = i;
+                    }
+                }
+
+                SteamCdmPath = temp.Substring(0, tempIndex);
+                SteamCMDName.Text = SteamCdmPath;
+            }
+        }
+
+        private void NumberOfPlayers_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void CSMaps_Initialized(object sender, EventArgs e)
+        {
+            CSMaps.ItemsSource = Maps;
+            CSMaps.SelectedIndex = 0;
+        }
+
+        private void CSMaps_GotFocus(object sender, RoutedEventArgs e)
+        {
+            MapCode = GetModes(CSMaps.SelectionBoxItem.ToString());
+        }
+
+        private void NumberOfPlayers_Initialized(object sender, EventArgs e)
+        {
+            NumberOfPlayers.Text = MaxPlayers.ToString();
+        }
+
+        private void NumberOfPlayers_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MaxPlayers = Int32.Parse(NumberOfPlayers.Text);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            UpdateClient();
+        }
+
+        private void UpdateClient()
+        {
+            Process CSUpdate = new Process();
+
+            try
+            {
+                CSUpdate.StartInfo.FileName = SteamCdmPath + "\\steamcmd.exe";
+                CSUpdate.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+                CSUpdate.StartInfo.Arguments = "+login anonymous +app_update 740 +quit";
+                CSUpdate.Start();
+                LastUpdated = DateTime.Now;
+                updateButt.Content = LastUpdated.ToString();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
         }
     }
 }
